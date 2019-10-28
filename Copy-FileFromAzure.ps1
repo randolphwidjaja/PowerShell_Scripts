cad885dab67bff90ea7ce8bd56c29e305fdea69d
Randolph Widjaja <randolph.widjaja@centric.eu>
2019-10-28 15:15:39 +0100

    Add files via upload

--- Copy-FileFromAzure.ps1
+++ Copy-FileFromAzure.ps1
@@ -20,45 +20,44 @@ filter Timestamp {"$(Get-Date -Format G): $_"}
 If (!$SkipToPhaseTwo)
 { 
   
- "Script started successfully" | Timestamp |  Out-File -FilePath $OutputResult -Append
+	"Script started successfully" | Timestamp |  Out-File -FilePath $OutputResult -Append
     
-Invoke-WebRequest "https://***.blob.core.windows.net/***/microsoft-windows-netfx3-ondemand-package.cab" -OutFile "C:\WindowsAzure\Applications\microsoft-windows-netfx3-ondemand-package.cab"
-Invoke-WebRequest "https://***.blob.core.windows.net/***/npp.7.7.1.Installer.exe" -OutFile "C:\WindowsAzure\Applications\npp.7.7.1.Installer.exe"
+	Invoke-WebRequest "https://***.blob.core.windows.net/***/microsoft-windows-netfx3-ondemand-package.cab" -OutFile "C:\WindowsAzure\Applications\microsoft-windows-netfx3-ondemand-package.cab"
+	Invoke-WebRequest "https://***.blob.core.windows.net/***/npp.7.7.1.Installer.exe" -OutFile "C:\WindowsAzure\Applications\npp.7.7.1.Installer.exe"
 
 
-  # This code schedules the currently running script to restart and forward to a given checkpoint:
-  schtasks.exe /create /tn "HeadlessRestartTask" /ru *** /sc ONSTART /tr "powershell.exe Start-Process -filepath $PostInstallationScriptLocation\Copy-FileFromAzure.ps1 -SkipToPhaseTwo"
-  Write-Host "Copy-FileFromAzure.ps1 Installation script is scheduled to run once after reboot."
-  "HeadlessRestartTask scheduler succesfully created" | Timestamp |  Out-File -FilePath $OutputResult -Append
+	# A task is created that starts the script after reboot and forward to the second part of the script.
+	schtasks.exe /create /tn "HeadlessRestartTask" /ru *** /sc ONSTART /tr "powershell.exe Start-Process -filepath $PostInstallationScriptLocation\Copy-FileFromAzure.ps1 -SkipToPhaseTwo"
+	"HeadlessRestartTask scheduler succesfully created" | Timestamp |  Out-File -FilePath $OutputResult -Append
 
 
-  # Installation Prerequirements
-  Set-ExecutionPolicy UnRestricted -Force
+	# Installation Prerequirements
+	Set-ExecutionPolicy UnRestricted -Force
 
-  If ($PSVersionTable.PSVersion | Where {$_.Major -ne "5"})
-  {
-	"PowerShellVersion:" | Out-File -FilePath $OutputResult -Append
-	$PSVersionTable.PSVersion |  Out-File -FilePath $OutputResult -Append
-	"Wrong Powershell version, please upgrade Powershell to V5.0" | Timestamp| Out-File -FilePath $OutputResult -Append
-	"Script failed" | Timestamp | Out-File -FilePath $OutputResult -Append
-	Break
-  }
+	If ($PSVersionTable.PSVersion | Where {$_.Major -ne "5"})
+	  {
+		"PowerShellVersion:" | Out-File -FilePath $OutputResult -Append
+		$PSVersionTable.PSVersion |  Out-File -FilePath $OutputResult -Append
+		"Wrong Powershell version, please upgrade Powershell to V5.0" | Timestamp| Out-File -FilePath $OutputResult -Append
+		"Script failed" | Timestamp | Out-File -FilePath $OutputResult -Append
+		Break
+	  }
 
-  $CheckFramework = Get-WindowsFeature | Where {$_.DisplayName -eq ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"}
-  If ($CheckFramework | Where {$_.InstallState -eq "Removed" -or "Available"})
-  {
+	$CheckFramework = Get-WindowsFeature | Where {$_.DisplayName -eq ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"}
+	If ($CheckFramework | Where {$_.InstallState -eq "Removed" -or "Available"})
+	  {
 
-	Install-WindowsFeature Net-Framework-Core -source "C:\WindowsAzure\Applications\"
-	Get-WindowsFeature | Where {$_.DisplayName -eq ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"} | Out-File -FilePath $OutputResult -Append
-  } 
-  Else
-  {
-	Get-WindowsFeature | Where {$_.DisplayName -eq ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"} | Out-File -FilePath $OutputResult -Append
-    ".NET Framework 3.5 (includes .NET 2.0 and 3.0) Installed" | timestamp |  Out-File -FilePath $OutputResult -Append
-  }
-    
-	
-  Shutdown /r /t 100
+		Install-WindowsFeature Net-Framework-Core -source "C:\WindowsAzure\Applications\"
+		Get-WindowsFeature | Where {$_.DisplayName -eq ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"} | Out-File -FilePath $OutputResult -Append
+	  } 
+	Else
+	  {
+		Get-WindowsFeature | Where {$_.DisplayName -eq ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"} | Out-File -FilePath $OutputResult -Append
+		".NET Framework 3.5 (includes .NET 2.0 and 3.0) Installed" | timestamp |  Out-File -FilePath $OutputResult -Append
+	  }
+		
+		
+	Shutdown /r /t 100
 } 
 
 
@@ -71,11 +70,11 @@ Start-Process schtasks.exe -ArgumentList "/delete /f /tn HeadlessRestartTask" -W
 # Install Notepad++ part Two
 If ((gp HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | ? { $_.DisplayName -eq "Notepad++ (32-bit x86)"}) -eq $null)
 {
-  Start-Process -FilePath "C:\WindowsAzure\Applications\npp.7.7.1.Installer.exe" -ArgumentList "/S" -PassThru -Wait
+	Start-Process -FilePath "C:\WindowsAzure\Applications\npp.7.7.1.Installer.exe" -ArgumentList "/S" -PassThru -Wait
 	Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize | Out-File -FilePath $OutputResult -Append
-  "Notepad++ installed" | Timestamp | Out-File -FilePath $OutputResult -Append
+	"Notepad++ installed" | Timestamp | Out-File -FilePath $OutputResult -Append
 }
 
 # Finish
 "Script Finished" | Timestamp| Out-File -FilePath $OutputResult -Append
- Shutdown /r /t 100
+Shutdown /r /t 100
